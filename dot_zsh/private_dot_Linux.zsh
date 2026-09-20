7d77b906e159f3f5b04adb47e2b907e0b9fa0fbc
Yen-Chi Chen <zxkyjimmy@gmail.com>
2026-09-21 01:26:00 +0800

    uv completion

--- dot_zsh/private_dot_Linux.zsh
+++ dot_zsh/private_dot_Linux.zsh
@@ -1,5 +1,3 @@
-export PATH="$HOME/.local/bin:$PATH"
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$(''$HOME'/.miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
